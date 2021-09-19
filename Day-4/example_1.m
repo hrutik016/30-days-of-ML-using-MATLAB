1a7af8dac3de8633aa9cb5f665b22fd782ca4a18
hrutik016 <hrutik716@gmail.com>
2021-09-19 23:53:07 +0530

    day-4

--- Day-4/example_1.m
+++ Day-4/example_1.m
@@ -22,7 +22,7 @@ function varargout = example_1(varargin)
 
 % Edit the above text to modify the response to help example_1
 
-% Last Modified by GUIDE v2.5 19-Sep-2021 01:02:51
+% Last Modified by GUIDE v2.5 19-Sep-2021 18:17:48
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -123,7 +123,8 @@ B = imadd(A, val);
 axes(handles.axes2);
 imshow(B);
 disp(val);
-
+val=num2str(val);
+set(handles.edit1,'String',val);
 
 
 % --- Executes during object creation, after setting all properties.
@@ -136,3 +137,144 @@ function slider1_CreateFcn(hObject, eventdata, handles)
 if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
 end
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on selection change in popupmenu1.
+function popupmenu1_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu1
+
+%displays image according to the val from pop-up box
+val = get(hObject,'Value');
+
+switch val
+          case 1
+            A = imread('cameraman.tif');
+            axes(handles.axes1);
+            imshow(A);
+          case 2
+            A = imread('coins.png');
+            axes(handles.axes1);
+            imshow(A);
+          case 3
+            A = imread('rice.png');
+            axes(handles.axes1);
+            imshow(A);
+          otherwise
+            disp('Unknown method.')
+        end
+
+disp(val);
+
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on selection change in listbox1.
+function listbox1_Callback(hObject, eventdata, handles)
+% hObject    handle to listbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns listbox1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from listbox1
+
+%code for pop-up, works same for list box.
+val = get(hObject,'Value');
+
+switch val
+          case 1
+            A = imread('cameraman.tif');
+            axes(handles.axes1);
+            imshow(A);
+          case 2
+            A = imread('coins.png');
+            axes(handles.axes1);
+            imshow(A);
+          case 3
+            A = imread('rice.png');
+            axes(handles.axes1);
+            imshow(A);
+          otherwise
+            disp('Unknown method.')
+        end
+
+disp(val);
+
+% --- Executes during object creation, after setting all properties.
+function listbox1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to listbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: listbox controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in radiobutton1.
+function radiobutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of radiobutton1
+val = get(hObject,'Value');
+
+if val == 1
+    A = imread('cameraman.tif');
+    axes(handles.axes1);
+    imshow(A);
+else
+   A = imread('rice.png');
+   axes(handles.axes1);
+   imshow(A); 
+end
+
+
+% --- Executes on button press in checkbox1.
+function checkbox1_Callback(hObject, eventdata, handles)
+% hObject    handle to checkbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of checkbox1

6212f8d0f70172271e8b3fc8f5171ab19b9b9ba0
hrutik016 <hrutik716@gmail.com>
2021-11-06 11:59:45 +0530

    updated

--- Day-16/example_5.m
+++ Day-16/example_5.m
@@ -1,9 +1,7 @@
 clc;
 clear all;
 cloase all;
-
 camObj = webcam(1);
-
 preview(camObj);
 
 i=0;
@@ -12,24 +10,19 @@ while(1)
     img = snapshot(camObj);
     imshow(img);
 
-    %bounding box around the target
     bbox = step(faceDetector, img);
     
     if ~isempty(bbox)
         bbox = bbox(1,:);
+        
+        face = imcrop(img, bbox);
+        face = imresize(face, [227 227]);
+        class = classify(net, face);
         IFaces = insertObjectAnnotation(img, 'rectangle', bbox, 'Face');
-        face = imcrop(img,bbox);
 
-        %to save the image
-        filename = strcat(num2str(i), '.bmp');
-        imwrite(face,filename);
-
-        i=i+1;
         imshow(IFaces);
     end
 end
 
-%after this create a folder and save around 50 snapshots from this, to
-%create a database.
-
-%create 2 such databases of different face images.
+% further we can go to deep network designer app in MATLAB, to see all the
+% pretrained model available there.
